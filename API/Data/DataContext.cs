2fe4789e0a0bcfd1c93ef1db9608c5922d17b4d7
Korisnik <mirkomacanovic@yahoo.com>
2022-11-24 00:13:24 +0100

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>().HasData(
+                new AppUser() { Id = 1, UserName = "Pera" },
+                new AppUser() { Id = 2, UserName = "Mika" },
+                new AppUser() { Id = 3, UserName = "Ana" }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
